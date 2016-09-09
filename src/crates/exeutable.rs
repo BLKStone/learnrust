85df64141367bb2fd168b80f9290c58011f670db
BLKStone <register27@163.com>
2016-09-09 16:38:39 +0800

    modules and crates

---
+++ src/crates/exeutable.rs
@@ -0,0 +1,12 @@
+// executable.rs
+// Link to `library`, import items under the `rary` module
+extern crate rary;
+
+fn main() {
+    rary::public_function();
+
+    // Error! `private_function` is private
+    //rary::private_function();
+
+    rary::indirect_access();
+}
